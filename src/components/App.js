10ac23da11db4fdfc543aa4538fb9a76786d347d
niikhlsharma <65024567+niikhlsharma@users.noreply.github.com>
2024-02-22 16:29:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,24 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+
+ const [var1,set] = useState('false')
+ function fun(){
+  set(!var1)
+ }
  
   return (
     <div id="main">
 
     <button id='click' onClick={fun}>Render</button>
 
-    <p>
-
-
-    </p>
+    {var1 && <p id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
    
     </div>
   );
 }
 
-const para = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
-  function fun()
-{
-   const abc = document.getElementsByTagName("p")
-   abc.innerHTML = para;
-   abc.id = 'para';
-}
+
+  
 
 export default App;
